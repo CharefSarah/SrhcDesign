1a1568c5e139d99fd386cf59ba839802264de5ab
SarahCharef <charef.sarah@gmx.fr>
2023-05-21 14:59:53 +0200

    pull pull

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,113 +1,129 @@
-  // Définition des variables et initialisation de l'animation SplitType
-  let typeSplit;
+// Définition des variables et initialisation de l'animation SplitType
+let typeSplit;
 
-  function runSplit() {
-    typeSplit = new SplitType(".split__lines", {
-      types: "lines, words"
-    });
-    $(".line").append("<div class='line__mask'></div>");
-    createAnimation();
+function runSplit() {
+  typeSplit = new SplitType(".split__lines", {
+    types: "lines, words"
+  });
+  $(".line").append("<div class='line__mask'></div>");
+  createAnimation();
+}
+
+// Gestion du redimensionnement de la fenêtre
+let windowWidth = $(window).innerWidth();
+window.addEventListener("resize", function () {
+  if (windowWidth !== $(window).innerWidth()) {
+    windowWidth = $(window).innerWidth();
+    typeSplit.revert();
+    runSplit();
   }
+});
 
-  // Gestion du redimensionnement de la fenêtre
-  let windowWidth = $(window).innerWidth();
-  window.addEventListener("resize", function () {
-    if (windowWidth !== $(window).innerWidth()) {
-      windowWidth = $(window).innerWidth();
-      typeSplit.revert();
-      runSplit();
-    }
+// Création de l'animation avec GSAP et ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+function createAnimation() {
+  $(".line").each(function (index) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: $(this),
+        start: "top 70%",
+        end: "bottom bottom",
+        scrub: 4
+      }
+    });
+    let delay = index * 2;
+    tl.to($(this).find(".line__mask"), {
+      width: "0%",
+      duration: 3,
+      delay: delay
+    });
+  });
+}
+
+window.onload = function () {
+  runSplit();
+
+  // Sélectionnez les éléments que vous voulez animer
+  const h1 = document.querySelector('h1');
+  const h2__content = document.querySelectorAll('.title span');
+  const links = document.querySelectorAll('li a');
+  const skills = document.querySelectorAll('.skills li');
+
+  // Commencez l'animation pour le h2 dès que la page est chargée
+  gsap.to(h2__content, {
+    rotateX: 0,
+    translateY: 0,
+    delay: 1,
+    duration: .7,
+    stagger: .02,
+    opacity: 1,
+    ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
   });
 
-  // Création de l'animation avec GSAP et ScrollTrigger
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.from(h1, {
+    delay: 1.8,
+    duration: 0.3,
+    autoAlpha: 0,
+    ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
+    y: -20
+  });
 
-  function createAnimation() {
-    $(".line").each(function (index) {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: $(this),
-          start: "top 70%",
-          end: "bottom bottom",
-          scrub: 4
-        }
-      });
-      let delay = index * 2;
-      tl.to($(this).find(".line__mask"), {
-        width: "0%",
-        duration: 3,
-        delay: delay
-      });
-    });
-  }
-
-  window.onload = function () {
-    runSplit();
-
-    // Sélectionnez les éléments que vous voulez animer
-    const h1 = document.querySelector('h1');
-    const h2 = document.querySelector('.title');
-    const links = document.querySelectorAll('li a');
-    const skills = document.querySelectorAll('.skills li');
-
-    // Commencez l'animation pour le h2 dès que la page est chargée
-    gsap.from(h2, {
-      delay: 0.2,
-      duration: 1,
+  // Animer chaque lien individuellement
+  links.forEach((link, index) => {
+    gsap.from(link, {
+      delay: 1.8 + index * 0.2,
+      duration: 0.3,
       autoAlpha: 0,
-      ease: "power4.out",    y: -50
+      ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
+      y: -20
     });
-  
-    // Animer le h1 après un court délai
-    gsap.from(h1, {
-      delay: 1.5,
-      duration: 0.6,
-      autoAlpha: 0,
-      ease: "power4.out",
-      y: -50
-    });
-  
-    // Animer chaque lien individuellement
-    links.forEach((link, index) => {
-      gsap.from(link, {
-        delay: 1.5 + index * 0.3,
-        duration: 0.6,
-        autoAlpha: 0,
-        ease: "power4.out",
-        y: -50
-      });
-    });
-  
-    // Animer chaque compétence individuellement
-    skills.forEach((skill, index) => {
-      gsap.from(skill, {
-        delay: 1.5 + index * 0.3,
-        duration: 0.2,
-        autoAlpha: 0,
-        ease: "power4.out",
-        y: -50
-      });
-    });
-  };
+  });
+};
 
-  function Marquee(selector, speed) {
-    const parentSelector = document.querySelector(selector);
-    const clone = parentSelector.innerHTML;
-    const firstElement = parentSelector.children[0];
-    let i = 0;
-    console.log(firstElement);
-    parentSelector.insertAdjacentHTML('beforeend', clone);
-    parentSelector.insertAdjacentHTML('beforeend', clone);
-  
-    setInterval(function () {
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  let isMouseOver = false; // Variable pour suivre l'état de survol de la souris
+
+  // Arrêter le défilement lorsque la souris survole la section
+  parentSelector.addEventListener('mouseover', function () {
+    isMouseOver = true;
+  });
+
+  // Reprendre le défilement lorsque la souris quitte la section
+  parentSelector.addEventListener('mouseout', function () {
+    isMouseOver = false;
+  });
+
+  // Arrêter le défilement lorsque la souris survole les boutons
+  const buttons = document.querySelectorAll('.button__marquee');
+  buttons.forEach(function (button) {
+    button.addEventListener('mouseover', function () {
+      isMouseOver = true;
+    });
+
+    button.addEventListener('mouseout', function () {
+      isMouseOver = false;
+    });
+  });
+
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+
+  setInterval(function () {
+    if (!isMouseOver) {
       firstElement.style.marginLeft = `-${i}px`;
       if (i > firstElement.clientWidth) {
         i = 0;
       }
       i = i + speed;
-    }, 0);
-  }
-  
- 
-  window.addEventListener('load', Marquee('.marquee', 0.2))
-  
+    }
+  }, 0);
+}
+
+window.addEventListener('load', function () {
+  Marquee('.marquee', 0.4);
+});
