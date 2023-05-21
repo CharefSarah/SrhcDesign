6e50a373e89671e260d697db817abded83386112
William Touboul <Touboul.william@gmail.com>
2023-05-21 14:40:49 +0200

    anim

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,44 +47,48 @@
     // Sélectionnez les éléments que vous voulez animer
     const h1 = document.querySelector('h1');
     const h2 = document.querySelector('.title');
+    const h2__content = document.querySelectorAll('.title span');
     const links = document.querySelectorAll('li a');
     const skills = document.querySelectorAll('.skills li');
 
     // Commencez l'animation pour le h2 dès que la page est chargée
-    gsap.from(h2, {
-      delay: 0.2,
-      duration: 1,
-      autoAlpha: 0,
-      ease: "power4.out",    y: -50
+    gsap.to(h2__content, {
+      rotateX: 0,
+      translateY: 0,
+      delay: 1.2,
+      duration: .8,
+      stagger: .02,
+      opacity: 1,
+      ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
     });
-  
+
     // Animer le h1 après un court délai
     gsap.from(h1, {
-      delay: 1.5,
-      duration: 0.6,
+      delay: 1.8,
+      duration: 0.8,
       autoAlpha: 0,
-      ease: "power4.out",
-      y: -50
+      ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
+      y: -20
     });
-  
+
     // Animer chaque lien individuellement
     links.forEach((link, index) => {
       gsap.from(link, {
-        delay: 1.5 + index * 0.3,
-        duration: 0.6,
+        delay: 1.8 + index * 0.2,
+        duration: 0.8,
         autoAlpha: 0,
-        ease: "power4.out",
-        y: -50
+        ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
+        y: -20
       });
     });
-  
+
     // Animer chaque compétence individuellement
     skills.forEach((skill, index) => {
       gsap.from(skill, {
-        delay: 1.5 + index * 0.3,
-        duration: 0.2,
+        delay: 1.8 + index * 0.2,
+        duration: 0.8,
         autoAlpha: 0,
-        ease: "power4.out",
+        ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
         y: -50
       });
     });
@@ -98,7 +102,7 @@
     console.log(firstElement);
     parentSelector.insertAdjacentHTML('beforeend', clone);
     parentSelector.insertAdjacentHTML('beforeend', clone);
-  
+
     setInterval(function () {
       firstElement.style.marginLeft = `-${i}px`;
       if (i > firstElement.clientWidth) {
@@ -107,7 +111,26 @@
       i = i + speed;
     }, 0);
   }
-  
- 
+
+
   window.addEventListener('load', Marquee('.marquee', 0.2))
-  
+
+
+
+  const callto = document.querySelector('.reveal__callto');
+  const callto__letters = document.querySelectorAll('.callto__letters');
+
+  gsap.to(callto__letters, {
+    scrollTrigger: {
+      trigger: callto,
+      start: "top 55%",
+      end: "top 20%",
+    },
+    rotateX: 0,
+    translateY: 0,
+    duration: .6,
+    stagger: .016,
+    opacity: 1,
+    ease: CustomEase.create("custom", "M0,0 C0.435,0.25 0.15,0.965 1,1 "),
+
+  })
